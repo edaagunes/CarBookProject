560af24b0cbda02cd73b1dc59e722ca46c6a53f5
edaagunes <edagunes00@hotmail.com>
2025-03-19 14:12:28 +0300

    Added Banner and About Crud transactions in Admin area

--- Presentation/CarBook.WebApi/Controllers/AboutsController.cs
+++ Presentation/CarBook.WebApi/Controllers/AboutsController.cs
@@ -44,7 +44,7 @@ namespace CarBook.WebApi.Controllers
 			await _createAboutCommandHandler.Handle(command);
 			return Ok();
 		}
-		[HttpDelete]
+		[HttpDelete("{id}")]
 		public async Task<IActionResult> RemoveAbout(int id)
 		{
 			await _removeAboutCommandHandler.Handle(new RemoveAboutCommand(id));

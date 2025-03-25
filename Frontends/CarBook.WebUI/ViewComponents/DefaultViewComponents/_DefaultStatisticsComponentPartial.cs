faae389296c415b041c6f18884a4d4dcd2e2d8df
edaagunes <edagunes00@hotmail.com>
2025-03-25 13:25:14 +0300

    Added Statistics in default page and Edited Footer

--- Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
@@ -1,11 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.StatisticDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CarBook.WebUI.ViewComponents.DefaultViewComponents
 {
-	public class _DefaultStatisticsComponentPartial:ViewComponent
+	public class _DefaultStatisticsComponentPartial : ViewComponent
 	{
-		public IViewComponentResult Invoke()
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _DefaultStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
 		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7053/api/Statistics/AllStatistics");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<AllStatisticDto>(jsonData);
+
+				ViewBag.carCount = values.CarCount;
+				ViewBag.locationCount = values.LocationCount;
+				ViewBag.brandCount = values.BrandCount;
+				ViewBag.carCountByFuelElectric = values.CarCountByFuelElectric;
+
+				return View(values);
+			}
 			return View();
 		}
 	}
